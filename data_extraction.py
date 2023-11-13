2c0795b1b802507278bd5e7c532a5008d7180699
elishagretton <elishagretton@gmail.com>
2023-11-13 22:47:52 +0000

    Updates data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,22 @@
+from database_utils import *
+import pandas as pd
+import sqlalchemy
+
+
 class DataExtractor():
     """
     This class extracts data from different data sources.
     e.g. CSV fies, API, S3 bucket.
     """
+    def __init__(self):
+        self.instance = DatabaseConnector()
+        self.db_engine = self.instance.db_engine
+        self.db_creds = self.instance.db_creds
+
+    def read_rds_table(self, table_name):
+        table_data = pd.read_sql_table(table_name, self.db_engine).set_index('index')
+        return table_data
+
+extractor = DataExtractor()
+
+print(extractor.read_rds_table('legacy_users'))
--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,46 @@
+import yaml
+from sqlalchemy import create_engine, inspect
+
+
 class DatabaseConnector():
     """
     This class connects and uploads data to a database.
     """
+
+    def __init__(self):
+        self.db_creds = self.read_db_creds()
+        self.db_engine = self.init_db_engine()
+        
+    def read_db_creds(self):
+        file_path = 'db_creds.yaml'
+        with open(file_path, 'r') as file:
+            db_creds = yaml.safe_load(file)
+        return db_creds
+    
+    def init_db_engine(self):
+        database_url = (
+        f"postgresql://{self.db_creds['RDS_USER']}:{self.db_creds['RDS_PASSWORD']}"
+        f"@{self.db_creds['RDS_HOST']}:{self.db_creds['RDS_PORT']}/{self.db_creds['RDS_DATABASE']}"
+        )
+        db_engine = create_engine(database_url)
+        return db_engine
+    
+    def list_db_tables(self):
+        inspector = inspect(self.db_engine)
+        db_table_list = inspector.get_table_names()
+        return db_table_list
+
+
+
+
+    
+instance = DatabaseConnector()
+
+# Step 2: Print dictionary of credentials.
+# print(instance.read_db_creds())
+
+# Step 3: Create init_db_engine method, return sqlalchemy DB engine.
+#instance.init_db_engine()
+
+# Step 4: Create method list_db_tables.
+#print(instance.list_db_tables())
